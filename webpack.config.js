ae4b5e0299215e5ac6be2af5d5a6085cb3748f18
PenguinLord561 <132022567+PenguinLord561@users.noreply.github.com>
2024-06-29 19:59:48 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 //const webpack = require("webpack");
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-
+const { CAPTCHASITE } = require("./config.json");
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
